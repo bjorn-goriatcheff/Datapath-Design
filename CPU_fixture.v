3593083465aa39ab34349a5d53a68da94422efc4
peckhamrachel <34068104+peckhamrachel@users.noreply.github.com>
2017-11-29 22:36:46 -0800

    Update CPU_fixture.v

--- CPU_fixture.v
+++ CPU_fixture.v
@@ -1,4 +1,33 @@
 `include "CPU.v"
 
 module CPU_fixture;
+
+// Declare variables for stimulating input
+reg clk, rst;
+
+initial
+        $vcdpluson;
+
+initial
+begin
+       $monitor();
+       $display(); //PC, ADDERS, MEMORY, REGISTER FILE, ALU, pipeline buffers, etc. on negedge
+end
+
+// Instantiate the CPU
+CPU c1(clk, rst);
+
+// Setup the clock to toggle every 10 time units
+initial
+begin
+       CLOCK = 1'b0;
+       forever #10 CLOCK = ~CLOCK;
+end
+
+// Finish the simulation at time 500
+initial
+begin
+       #500 $finish;
+end
+
 endmodule
