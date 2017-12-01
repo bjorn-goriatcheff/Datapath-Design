6a165651456459a9fe3dcaded80010ffdfb59034
bjorn-goriatcheff <bjorn.goriatcheff@u-psud.fr>
2017-11-30 18:39:12 -0800

    Read Register
    Combinatorial block written: 
    transform input as decimal count
    read from addess 1, 2, 15
    output data

--- Registers.v
+++ Registers.v
@@ -6,20 +6,33 @@ module Registers(
 	input [3:0] ReadAdd2,
 	input [3:0] WriteReg1,
 	input [3:0] WriteReg2,
+	input [3:0] WriteReg15,
 	input WriteDst,
 	input clk,
 	input rst
 );
 //15 Registers
 reg[15:0]Reg [15:0];
-
+integer count;
 //Combinatorial READ
 always@(*)
 begin
-
-
-
-
+	//Read Register from Address 1
+	count=0;
+	count=count+ReadAdd1[0];
+	count=count+ReadAdd1[1]*(2**1);
+	count=count+ReadAdd1[2]*(2**2);
+	count=count+ReadAdd1[3]*(2**3);
+	Data1=Reg[count];
+	//Read Register from Address 2
+	count=0;
+	count=count+ReadAdd2[0];
+	count=count+ReadAdd2[1]*(2**1);
+	count=count+ReadAdd2[2]*(2**2);
+	count=count+ReadAdd2[3]*(2**3);
+	Data2=Reg[count];
+	//Read Register 15 
+	Data15=Reg[15];
 
 end
 
