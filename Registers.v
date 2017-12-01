e3e9f46eabe336af5c07df7f9cd236171bd09b3f
bjorn-goriatcheff <bjorn.goriatcheff@u-psud.fr>
2017-11-30 18:05:06 -0800

    Registers Init

---
+++ Registers.v
@@ -0,0 +1,17 @@
+module Registers(
+	output reg[15:0] Data1,
+	output reg[15:0] Data2,
+	output reg[15:0] Data15,
+	input [3:0] ReadAdd1,
+	input [3:0] ReadAdd2,
+	input [3:0] WriteReg1,
+	input [3:0] WriteReg2,
+	input WriteDst
+);
+
+
+
+
+
+
+endmodule
