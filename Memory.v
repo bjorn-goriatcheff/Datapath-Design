2aeb99fc3207f5630cbfa1ec5295b9dc6fdd244c
peckhamrachel <34068104+peckhamrachel@users.noreply.github.com>
2017-12-02 17:39:41 -0800

    Questionable Progress

--- Memory.v
+++ Memory.v
@@ -1,25 +1,29 @@
 module Memory(
-	output reg[8:0] Byte,
+	output reg[7:0] Byte,
 	output reg[15:0] Word,
-	input [3:0] ReadAdd,
-	input [8:0] WriteByte,
-	input [15:0] WriteWord,
+	input [15:0] op1,
+	input [15:0] op2,
+	input [15:0] immd,
 	input [1:0] MemW,
 	input clk,
 	input rst
 );
-integer inc, count1;
+integer inc, inc1, inc2, count1, count2;
+reg[7:0] mem [99:0];
 
 //Combinatorial READ
 always@(*)
 begin
-	count1=0;
-	count1=count1+ReadAdd[0];
-	count1=count1+ReadAdd[1]*(2**1);
-	count1=count1+ReadAdd[2]*(2**2);
-	count1=count1+ReadAdd[3]*(2**3);
-	Byte=count; //?
+	count1=immd;
+	for(inc=0;inc<16;inc=inc+1)
+		count1=count1+op2[inc]*(2**inc);
+	Byte[7:0]=mem[count1];
 	
+	count2=immd;
+	for(inc2=0;inc2<16;inc2=inc2+1)
+		count2=count2+op2[inc2]*(2**inc2);
+	Word[7:0]=mem[count2];
+	Word[15:8]=mem[count2+1];
 end
 
 //Sequential WRITE
@@ -27,18 +31,19 @@ always@(posedge clk, negedge rst)
 begin
 	if(!rst)
 	begin
-		for(inc=0;inc<16;inc=inc+1) //?
-			
+		for(inc1=0;inc1<99;inc1=inc1+1)
+			mem[inc1]<=0;
 	end
 	else if(MemW==2'b01)
 	begin
 		//store byte at addr
-		
+		mem[count1]<=op1[7:0];
 	end
 	else if(MemW==2'b10)
 	begin
 		//store word at addr
-	
+		mem[count2]<=op1[7:0];
+		mem[count2+1]<=op1[15:8];	
 	end
 	
 end
