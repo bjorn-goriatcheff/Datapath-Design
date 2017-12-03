382a45d3e95c9e941cf5b3935d0f600f02125e91
peckhamrachel <34068104+peckhamrachel@users.noreply.github.com>
2017-12-02 19:18:59 -0800

    Write Word Fix

--- Memory.v
+++ Memory.v
@@ -1,29 +1,22 @@
-module Memory(
+module Memory#(parameter N=999)
+(
 	output reg[7:0] Byte,
 	output reg[15:0] Word,
-	input [15:0] op1,
-	input [15:0] op2,
-	input [15:0] immd,
+	input [15:0] Addr,
+	input [15:0] WriteW,
+	input [7:0] WriteB,
 	input [1:0] MemW,
 	input clk,
 	input rst
 );
-integer inc, inc1, inc2, count1, count2;
-reg[7:0] mem [99:0];
+integer inc;
+reg[15:0] mem [15:0]; //reg[7:0] ?
 
 //Combinatorial READ
 always@(*)
 begin
-	count1=immd;
-	for(inc=0;inc<16;inc=inc+1)
-		count1=count1+op2[inc]*(2**inc);
-	Byte[7:0]=mem[count1];
-	
-	count2=immd;
-	for(inc2=0;inc2<16;inc2=inc2+1)
-		count2=count2+op2[inc2]*(2**inc2);
-	Word[7:0]=mem[count2];
-	Word[15:8]=mem[count2+1];
+	Word=mem[Addr];
+	Byte=mem[Addr][7:0];
 end
 
 //Sequential WRITE
@@ -31,19 +24,18 @@ always@(posedge clk, negedge rst)
 begin
 	if(!rst)
 	begin
-		for(inc1=0;inc1<99;inc1=inc1+1)
-			mem[inc1]<=0;
+		for(inc=0;inc<N;inc=inc+1)
+			mem[inc]<=16'h0000;
+	end
+	if(MemW==2'b10)
+	begin
+		//store word at addr
+		mem[Addr]<=WriteW;	
 	end
 	else if(MemW==2'b01)
 	begin
 		//store byte at addr
-		mem[count1]<=op1[7:0];
-	end
-	else if(MemW==2'b10)
-	begin
-		//store word at addr
-		mem[count2]<=op1[7:0];
-		mem[count2+1]<=op1[15:8];	
+		mem[Addr][7:0]<=WriteB;
 	end
 	
 end
